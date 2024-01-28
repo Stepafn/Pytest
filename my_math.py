4377519c11f1a938a8847384b1a4c8e11bf8f2fb
Stepafn <138586684+Stepafn@users.noreply.github.com>
2024-01-28 13:14:57 +0100

    Update my_math.py

--- my_math.py
+++ my_math.py
@@ -1,4 +1,10 @@
 # my_math.py
 
 def add_numbers(num1: int | float, num2: int | float) -> int | float:
-    return round(num1 + num2, max(len(str(num1)), len(str(num2))))
+    num1_floating_length = 0
+    num2_floating_length = 0
+    if isinstance(num1, float):
+        num1_floating_length = len(str(num1).split('.')[-1])
+    if isinstance(num2, float):
+        num2_floating_length = len(str(num2).split('.')[-1])
+    return round(num1 + num2, max(num1_floating_length, num2_floating_length))
